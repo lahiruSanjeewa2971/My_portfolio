df56ddab1ac599cb5830eb8692d5db5400107523
SYNAPSYS\LahiruR <lahiru.rathnasheela@synapsys.lk>
2024-06-30 15:12:42 +0530

    initial

---
+++ components/Social.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/lahiruSanjeewa2971" },
+  {
+    icon: <FaLinkedin />,
+    path: "https://www.linkedin.com/in/lahiru-sanjeewa-180910201/",
+  },
+];
+
+const Social = ({ containerStyle, iconStyle }) => {
+  return (
+    <div className={containerStyle}>
+      {socials.map((icon, index) => {
+        return (
+          <Link key={index} href={icon.path} className={iconStyle}>
+            {icon.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
