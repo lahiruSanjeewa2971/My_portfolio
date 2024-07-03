afcb2bc38309c60195acfc974387178d6b80abfb
SYNAPSYS\LahiruR <lahiru.rathnasheela@synapsys.lk>
2024-07-03 19:34:21 +0530

    finish the project, remaining to change it for me

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Photo from "@/components/Photo";
 import Social from "@/components/Social";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -8,7 +9,7 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br />{" "}
@@ -16,7 +17,7 @@ const Home = () => {
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I'm a dedicated software engineer with two years of hands-on
-              experience in React and React Native. Passionate about creating
+              experience in React, React Native and nodejs. Passionate about creating
               seamless user experiences, I've honed my skills in building
               dynamic and responsive applications. With a knack for
               problem-solving and a commitment to continuous learning, I strive
@@ -41,11 +42,12 @@ const Home = () => {
             </div>
           </div>
 
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client";
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim maiores vero facere ullam",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "Mobile App Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim maiores vero facere ullam",
+    href: "",
+  },
+];
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-1 flex-col justify-center gap-6 group"
+              >
+                <div className="flex w-full justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                    {service.num}
+                  </div>
+                  <Link
+                    href={service.href}
+                    className="flex w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
+                  </Link>
+                </div>
 
-export default Services
+                <h2 className="text-[32px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </h2>
+                <p>{service.description}</p>
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
